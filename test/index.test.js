007e31dcef6a1705f85215c524c2249559e760e9
J Gogoi <codergogoi@gmail.com>
2021-01-10 20:52:19 +0530

    Added Test

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('Sameple test', function(){
+    it('should be equal', function(){
+        const x = 10;
+        const y = 10;
+        expect(x).to.equal(y);
+    })
+
+});
